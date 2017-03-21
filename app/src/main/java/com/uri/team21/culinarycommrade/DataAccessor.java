1e9dd33c545358275c00e52a2e69b8d7883d9c07
Douglas Deslauriers <superpowerkid44@gmail.com>
2017-03-20 21:43:58 -0400

    Removed invisible characters

--- app/src/main/java/com/uri/team21/culinarycommrade/DataAccessor.java
+++ app/src/main/java/com/uri/team21/culinarycommrade/DataAccessor.java
@@ -1,9 +1,9 @@
 package com.uri.team21.culinarycommrade;
 
-import android.database.Cursor;
 import android.content.Context;
-import java.util.*;
+import android.database.Cursor;
 
+import java.util.ArrayList;
 
 
 public class DataAccessor {
@@ -23,7 +23,7 @@ public class DataAccessor {
 
         ArrayList<String> data = new ArrayList<>();
         Cursor cursor = rDbHelper.query("SELECT _description FROM esha");
-        while(!cursor.isAfterLast()) {
+        while (!cursor.isAfterLast()) {
             data.add(cursor.getString(cursor.getColumnIndex("_description")));
             cursor.moveToNext();
         }
@@ -37,7 +37,7 @@ public class DataAccessor {
 
         ArrayList<String> data = new ArrayList<>();
         Cursor cursor = iDbHelper.query("SELECT Ingredient FROM Inventory");
-        while(!cursor.isAfterLast()) {
+        while (!cursor.isAfterLast()) {
             data.add(cursor.getString(cursor.getColumnIndex("Ingredient")));
             cursor.moveToNext();
         }
@@ -55,7 +55,7 @@ public class DataAccessor {
         cursor.close();
         Cursor cursor2 = lDbHelper.query("SELECT Ingredient,Recipe FROM List");
         int i = 0;
-        while(!cursor2.isAfterLast()) {
+        while (!cursor2.isAfterLast()) {
             data[i][0] = cursor2.getString(cursor2.getColumnIndex("Ingredient"));
             data[i][1] = cursor2.getString(cursor2.getColumnIndex("Recipe"));
             cursor2.moveToNext();
@@ -66,66 +66,69 @@ public class DataAccessor {
 
     }
 
-     public ArrayList<String> getIngredients(String Name) { 
+
+    public ArrayList<String> getIngredients(String Name) {
         String query = "SELECT RecipeItem/0/_ItemName";
-             for( int i=1; i<25; i++)     {         query+=", RecipeItem/"+i+"/_ItemName";
-             }     query+= "FROM esha";
-             query+= "WHERE _description ="+ Name;
-             ArrayList<String> data = new ArrayList<>();
-             Cursor cursor = rDbHelper.query(query);
-              for( int i=0; i<25; i++)     {
-               String someData=  cursor.getString(cursor.getColumnIndex("RecipeItem/"+i+"/_ItemName"));
-                 if (someData == null) {
-                     break; 
-            } 
-                else  { 
-                data.add(someData); 
-            } 
-        }     cursor.close();
-              return data;  
+        for (int i = 1; i < 25; i++) {
+            query += ", RecipeItem/" + i + "/_ItemName";
+        }
+        query += "FROM esha";
+        query += "WHERE _description =" + Name;
+        ArrayList<String> data = new ArrayList<>();
+        Cursor cursor = rDbHelper.query(query);
+        for (int i = 0; i < 25; i++) {
+            String someData = cursor.getString(cursor.getColumnIndex("RecipeItem/" + i + "/_ItemName"));
+            if (someData == null) break;
+            data.add(someData);
+        }
+        cursor.close();
+        return data;
     }
 
-      public ArrayList<String> getAllIngredients() { 
-        ArrayList<String> allIngredients =new ArrayList<String>();
-             ArrayList<String> recipes = getRecipes();
-             int size = recipes.size(); 
-        for (int i = 0; i<size; i++)     {
-                 ArrayList<String> someIngredients = getIngredients(recipes[i]); 
-            allIngredients = concat(allIngredients,someIngredients);
-                 } 
+
+    public ArrayList<String> getAllIngredients() {
+        ArrayList<String> allIngredients = new ArrayList<String>();
+        ArrayList<String> recipes = getRecipes();
+        int size = recipes.size();
+        for (int i = 0; i < size; i++) {
+            ArrayList<String> someIngredients = getIngredients(recipes[i]);
+            allIngredients = concat(allIngredients, someIngredients);
+        }
         return allIngredients;
-         }
+    }
 
-          public String[] concat(String[] a, String[] b) { 
+
+    public String[] concat(String[] a, String[] b) {
         int aLen = a.length;
-             int bLen = b.length;
-             String[] c= new String[aLen+bLen];
-             System.arraycopy(a, 0, c, 0, aLen); 
-            System.arraycopy(b, 0, c, aLen, bLen); 
-        return c; 
-    }  
+        int bLen = b.length;
+        String[] c = new String[aLen + bLen];
+        System.arraycopy(a, 0, c, 0, aLen);
+        System.arraycopy(b, 0, c, aLen, bLen);
+        return c;
+    }
 
-    public int getYield(String Name){
-          String query ="SELECT Yeild/measure/_quantity From esha WHERE _description="+Name; 
+    public int getYield(String Name) {
+        String query = "SELECT Yeild/measure/_quantity From esha WHERE _description=" + Name;
         Cursor cursor = rDbHelper.query(query);
-             String someData=  cursor.getString(cursor.getColumnIndex("Yeild/measure/_quantity")); 
+        String someData = cursor.getString(cursor.getColumnIndex("Yeild/measure/_quantity"));
         int yield = Integer.parseInt(someData);
-             return yield;
-         }
+        return yield;
+    }
 
-          public String getDescription(String Name){  
-        String query ="SELECT memo/__cdata From esha WHERE _description="+Name; 
+
+    public String getDescription(String Name) {
+        String query = "SELECT memo/__cdata From esha WHERE _description=" + Name;
         Cursor cursor = rDbHelper.query(query);
-             String Directions=  cursor.getString(cursor.getColumnIndex("memo/__cdata"));
-             return Directions;
-         }
+        String Directions = cursor.getString(cursor.getColumnIndex("memo/__cdata"));
+        return Directions;
+    }
 
     public void addToInventory(String Ingredient) {
         iDbHelper.query("INSERT INTO Inventory (Ingredient) VALUES ('" + Ingredient + "');");
     }
 
     public void addToShoppingList(String Ingredient, String Recipe) {
-        lDbHelper.query("INSERT INTO List (Ingredient, Recipe) VALUES ('" + Ingredient + "','" + Recipe +"');");
+        lDbHelper.query("INSERT INTO List (Ingredient, Recipe) VALUES ('" + Ingredient + "','" + Recipe + "');");
     }
 
     public void removeFromInventory(String Ingredient) {
@@ -133,11 +136,13 @@ public class DataAccessor {
     }
 
     public void removeFromShoppingList(String Ingredient, String Recipe) {
-        lDbHelper.query("DELETE FROM List WHERE Ingredient='" + Ingredient + "' AND Recipe='" + Recipe +"';");
+        lDbHelper.query("DELETE FROM List WHERE Ingredient='" + Ingredient + "' AND Recipe='" + Recipe + "';");
     }
+
     public void deleteAllInventory() {
         iDbHelper.query("DELETE FROM Inventory");
     }
+
     public void deleteAllList(String Recipe) {
         lDbHelper.query("DELETE FROM List WHERE Recipe='" + Recipe + "';");
     }
