1717cdc63b7f1495a5d4d08734f0cd79ef189096
Douglas Deslauriers <superpowerkid44@gmail.com>
2017-03-27 15:27:26 -0400

    Created algorithm that returns an object that contains sorted arrays based on the percentage complete.

---
+++ app/src/main/java/com/uri/team21/culinarycommrade/WeightedMessenger.java
@@ -0,0 +1,59 @@
+package com.uri.team21.culinarycommrade;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class WeightedMessenger {
+    public String[] recipes;
+    public double[] weights;
+    private int index = 0;
+
+    WeightedMessenger(int length) {
+        recipes = new String[length];
+        weights = new double[length];
+    }
+
+    public void append(String recipe, double weight) {
+        recipes[index] = recipe;
+        weights[index] = weight;
+        index++;
+    }
+
+    public void sort() {
+        NameNumber [] zip = new NameNumber[Math.min(recipes.length,weights.length)];
+        for(int i = 0; i < zip.length; i++)
+        {
+            zip[i] = new NameNumber(recipes[i],weights[i]);
+        }
+
+        Arrays.sort(zip, new Comparator<NameNumber>() {
+
+            @Override
+            public int compare(NameNumber o1, NameNumber o2) {
+                return Double.compare(o1.number, o2.number);
+            }
+        });
+
+        for(int i = 0; i < zip.length; i++) {
+            recipes[i] = zip[i].name;
+            weights[i] = zip[i].number;
+        }
+
+    }
+
+
+
+}
+
+class NameNumber
+{
+
+    public NameNumber(String name, double n) {
+        this.name = name;
+        this.number = n;
+    }
+
+    public String name;
+    public double number;
+}
---
+++ app/src/main/java/com/uri/team21/culinarycommrade/WeightedSearch.java
@@ -0,0 +1,40 @@
+package com.uri.team21.culinarycommrade;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public class WeightedSearch {
+
+    private DataAccessor db;
+    private ArrayList<String> recipes;
+    private ArrayList<String> inventory;
+
+    public WeightedSearch(Context context) {
+        db = new DataAccessor(context);
+    }
+
+    public WeightedMessenger getSortedList() {
+        recipes = db.getRecipes();
+        inventory = db.getInventory();
+
+        WeightedMessenger weightedList = new WeightedMessenger(recipes.size());
+
+        int k = 0;
+        for(String i : recipes) {
+            ArrayList<String> ingredients = db.getIngredients(i);
+            int count = 0;
+            for(String j: ingredients) {
+                if (j.equals(i)) {
+                    count++;
+                }
+            }
+            weightedList.append(i, (double)count/ingredients.size());
+        }
+
+        weightedList.sort();
+
+        return weightedList;
+    }
+}
+
